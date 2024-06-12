0fad28213d1641cc43a7519d9d2c505c4629b68c
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-12 18:14:02 +0200

    added axios and created metod to get response to API

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,32 @@
 <script>
 import CardList from './CardList.vue';
+import axios from 'axios';
 export default{
     components: {
         CardList
     },
     data(){
         return{
-
+            cards:[],
         }
+    },
+    methods:{
+        getCards(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+            .then(function(response) {
+                console.log(response.data.data);
+                this.cards=response.data.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+            .finally(function () {
+                
+            });
+        }
+    },
+    created(){
+        this.getCards();
     }
 }
 </script>
