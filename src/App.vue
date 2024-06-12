923b9252c8c5184dc6d26bf28ad9cfa0771007a5
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-12 17:36:21 +0200

    created and  linked component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+
     export default{
+      components:{
+        AppHeader,
+        AppMain
+      },
       data(){
         return{
 
@@ -9,12 +16,13 @@
 </script>
 
 <template>
-  
+  <AppHeader/>
+  <AppMain/>
 </template>
 
 <style lang="scss">
+@use'../node_modules/bootstrap/scss/bootstrap.scss';
 @use'./styles/partials/variable' as *;
 @use'./styles/partials/mixins' as *;
 @use'./styles/general.scss';
-@use'../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
