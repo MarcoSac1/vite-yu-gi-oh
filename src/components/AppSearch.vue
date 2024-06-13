dcabeaf42906d077080186abd5f220433ec03c2e
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-13 16:57:16 +0200

    created dropDown element in dom

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -9,24 +9,27 @@ export default{
             
         }
     },
-    methods:{
-        
-    },
-    created(){
-        
+    props:{
+        archetypes:{
+            type: Array,
+            required: true
+        }
     }
+    
 }
 </script>
 
 <template >
-<div class="dropdown ">
-    <button class="btn btn-danger m-4  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Archetypes selector
-    </button>
-    <ul class="dropdown-menu dropdown-menu-dark">
-        <li><a class="dropdown-item active" href="#">Action</a></li>
-    </ul>
-</div>
+    <section class="container">
+        <div class="dropdown ">
+            <button class="btn btn-danger m-4  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Archetypes selector
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item active" v-for="(archetypes, index) in archetypes" :key="index" :archetypes="archetypes" href="#">{{ archetypes.archetype_name }}</a></li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <style scoped>
