e81ec0dd4dbfb33d5805e89c3502064b66d98472
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-13 17:52:47 +0200

    fixed code issue

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -21,13 +21,21 @@ export default{
 
 <template >
     <section class="container">
+    <!-- <div class="dropdown-center">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Archetypes selector
+        </button>
+        <ul class="dropdown-menu" style="">
+            <li  v-for="(archetypes, index) in archetypes" :key="index" :archetypes="archetypes"><a class="dropdown-item" href="#">{{ archetypes.archetype_name }}</a></li>
+        </ul>
+    </div> -->
         <div class="dropdown ">
             <button class="btn btn-danger m-4  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Archetypes selector
             </button>
             <ul class="dropdown-menu dropdown-menu-dark">
                 <li v-for="(archetypes, index) in archetypes" :key="index" :archetypes="archetypes">
-                    <a class="dropdown-item active"  href="#">{{ archetypes.archetype_name }}</a>
+                    <a class="dropdown-item "  href="#">{{ archetypes.archetype_name }}</a>
                 </li>
             </ul>
         </div>
