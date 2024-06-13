68dba070b945a492f625043a94189255f33aa140
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-13 16:07:54 +0200

    created new component

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default{
+    components: {
+        
+    },
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        
+    },
+    created(){
+        
+    }
+}
+</script>
+
+<template>
+    <section>
+
+    </section>
+</template>
+
+<style scoped>
+
+</style>
